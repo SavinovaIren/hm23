3c049d3318c8fb65b68cf3d4c933dd8954a6225c
SavinovaIren <irka887@gmail.com>
2022-09-01 20:27:45 +0300

    update be hm24 without mypy mistakes

--- app.py
+++ app.py
@@ -10,47 +10,44 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
 
-def do_cmd(cmd: str, value: str, data: list) -> list:
-
+def do_cmd(cmd: str, value: str, data: list) -> list[str]:
     if cmd == 'filter':
-        return list(filter(lambda record: value in record, data))
+        cmd_result = list(filter(lambda record: value in record, data))
     elif cmd == 'map':
         column = int(value)
         if column == 0:
-            return list(map(lambda record: record.split()[column], data))
+            cmd_result = list(map(lambda record: record.split()[column], data))
         elif column == 1:
-            return list(map(lambda record: record.split()[3:5], data))
+            cmd_result = list(map(lambda record: record.split()[3:5], data))
         elif column == 2:
-            return list(map(lambda record: " ".join(record.split()[5:]), data))
+            cmd_result = list(map(lambda record: " ".join(record.split()[5:]), data))
     elif cmd == 'unique':
-        return list(set(data))
+        cmd_result = list(set(data))
     elif cmd == 'sort':
-        return sorted(data, reverse=True)
+        cmd_result = sorted(data, reverse=True)
     elif cmd == 'regexp':
         png_list = list(filter(lambda rec: re.findall(value, rec), data))
-        return list(map(lambda record: " ".join(record.split()[:9]), png_list))
-    else:
-        raise BadRequest
+        cmd_result = list(map(lambda record: " ".join(record.split()[:9]), png_list))
+
+    return cmd_result
 
 
-def do_query(params: dict) -> list:
+def do_query(params: dict) -> list[str]:
     with open(os.path.join(DATA_DIR, params["file_name"])) as f:
         file_data = f.readlines()
-    try:
-       res = file_data
-       if "cmd1" in params.keys():
-           return do_cmd(params["cmd1"], params["value1"], res)
-       if "cmd2" in params.keys():
-           return do_cmd(params["cmd2"], params["value2"], res)
-       if "cmd3" in params.keys():
-           return do_cmd(params["cmd3"], params["value3"], res)
-       if "cmd4" in params.keys():
-           return do_cmd(params["cmd4"], params["value4"], res)
-       if "cmd5" in params.keys():
-           return do_cmd(params["cmd5"], params["value5"], res)
-    except TypeError:
-        raise TypeError
 
+    res = file_data
+    if "cmd1" in params.keys():
+        result = do_cmd(params["cmd1"], params["value1"], res)
+    elif "cmd2" in params.keys():
+        result = do_cmd(params["cmd2"], params["value2"], res)
+    elif "cmd3" in params.keys():
+        result = do_cmd(params["cmd3"], params["value3"], res)
+    elif "cmd4" in params.keys():
+        result = do_cmd(params["cmd4"], params["value4"], res)
+    elif "cmd5" in params.keys():
+        result = do_cmd(params["cmd5"], params["value5"], res)
+    return result
 
 
 @app.route("/perform_query", methods=["POST"])
